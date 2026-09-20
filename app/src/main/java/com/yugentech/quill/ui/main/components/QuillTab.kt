3fbf2301bc20d43dacaa9d0ac1e4db6755460eaf
caoye19970910-maker <caoye19970910@gmail.com>
2026-09-20 19:37:09 +0800

    refactor: reduce navigation to library and import

--- app/src/main/java/com/yugentech/quill/ui/main/components/QuillTab.kt
+++ app/src/main/java/com/yugentech/quill/ui/main/components/QuillTab.kt
@@ -2,7 +2,5 @@ package com.yugentech.quill.ui.main.components
 
 enum class QuillTab(val title: String) {
     Library("Library"),
-    Discover("Discover"),
-    Sources("Sources"),
-    Settings("More")
+    Sources("Import")
 }
